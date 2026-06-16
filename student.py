654f1a69b792682aa3725a511a2f472e53707e47
DEKUMO7 <debangshukumarmondal233217@gmail.com>
2026-06-16 20:23:36 +0530

    add input function

--- student.py
+++ student.py
@@ -1,13 +1,13 @@
 class student:
-    def __init__(self,name,age,rollno):
-        self.name = name
-        self.age = age
-        self.rollno = rollno
+    def __init__(self):
+        self.name = input("Enter Name: ")
+        self.age = int(input("Enter Age: "))
+        self.rollno = int(input("Enter Roll No: "))
     
     def display(self):
         print("Name:",self.name)
         print("Age:",self.age)
         print("Roll No:",self.rollno)
 # Example usage
-student1 = student("john",20,101)
+student1 = student()
 student1.display()
