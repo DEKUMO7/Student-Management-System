9daf286b5d660281e0853f6285b03bf4b326c699
DEKUMO7 <debangshukumarmondal233217@gmail.com>
2026-06-17 21:13:26 +0530

    add loop

--- student.py
+++ student.py
@@ -1,15 +1,16 @@
-class student:
-    def __init__(self):
-        self.name = input("Enter Name: ")
-        self.age = int(input("Enter Age: "))
-        self.rollno = int(input("Enter Roll No: "))
-    
-    def display(self):
-        print("Name:",self.name)
-        print("Age:",self.age)
-        print("Roll No:",self.rollno)
-# Example usage
-student1 = student()
+while True:
+    a = input("Do you want to add student detaails? (y/n):")
+    if a == "y":
+        class student:
+            def __init__(self):
+                self.name = input("Enter Name: ")
+                self.age = int(input("Enter Age: "))
+                self.rollno = int(input("Enter Roll No: "))
 
-with open("student.txt","a")as f:
-    f.write(f"{student1.name}\n{student1.age}\n{student1.rollno}\n")
+    elif a == "n":
+        break
+    
+    student1 = student()
+
+    with open("student.txt","a")as f:
+        f.write(f"Name: {student1.name}\nAge: {student1.age}\nRoll No: {student1.rollno}\n")
