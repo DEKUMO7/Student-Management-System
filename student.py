a6e28a27dc9ead076ea8e8a7ee49e0d2c996c62f
DEKUMO7 <debangshukumarmondal233217@gmail.com>
2026-06-23 20:14:24 +0530

    add input loop

--- student.py
+++ student.py
@@ -8,23 +8,39 @@ class student:
 student_no = 1
 
 while True:
-    name = input("Enter Name: ")
-    if name.lower() == "exit" or name.lower() == "stop" or name.lower() == "quit":
-        print("program stopped")
-        break
-    age = input("Enter Age: ")
-    if age.lower() == "exit" or age.lower() == "stop" or age.lower() == "quit":
-        print("program stopped")
-        break
-    rollno = input("Enter Roll No: ")
-    if rollno.lower() == "exit" or rollno.lower() == "stop" or rollno.lower() == "quit":
-        print("program stopped")
-        break
+    while True:
+        name = input("Enter Name: ").strip()
+        if name.lower() == "exit" or name.lower() == "stop" or name.lower() == "quit":
+            print("program stopped")
+            break
+        elif name == "":
+            print("pleace enetr name first...")
+        
+    while True:
+        age = input("Enter Age: ").strip()
+        if age.lower() == "exit" or age.lower() == "stop" or age.lower() == "quit":
+            print("program stopped")
+            break
+        elif age == "":
+            print("pleace enetr age first...")
     
-    course = input("Enter Course: ")
-    if course.lower() == "exit" or course.lower() == "stop" or course.lower() == "quit":
-        print("program stopped")
-        break
+    while True:
+        rollno = input("Enter Roll No: ").strip()
+        if rollno.lower() == "exit" or rollno.lower() == "stop" or rollno.lower() == "quit":
+            print("program stopped")
+            break
+        elif rollno == "":
+            print("pleace enetr rollno first...")
+    
+    while True:
+        course = input("Enter Course: ").strip()
+        if course.lower() == "exit" or course.lower() == "stop" or course.lower() == "quit":
+            print("program stopped")
+            break
+        elif course == "":
+            print("pleace enetr course first...")
+    
+
     student1 = student(name, age, rollno, course)
 
     with open("student.txt","a")as file:
