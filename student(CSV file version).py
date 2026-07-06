075d5a472e65389bea91a803dcaa9a8927f01065
DEKUMO7 <debangshukumarmondal233217@gmail.com>
2026-07-06 21:27:12 +0530

    add loop in enter data

--- student(CSV file version).py
+++ student(CSV file version).py
@@ -1,10 +1,33 @@
 import csv
 
 def add_student():
-    name = input("Enter Student Name: ")
-    roll = input("Enter Student RollNo: ")
-    age = input("Enter Student Age: ")
-    course = input("Enter Course: ")
+    while True:
+        name = input("Enter Student Name: ").strip()
+        if name == "":
+            print("Please enter a name first...")
+        else:
+            break
+    
+    while True:
+        roll = input("Enter Student RollNo: ").strip()
+        if roll == "":
+            print("Please enter a roll number first...")
+        else:
+            break
+
+    while True:
+        age = input("Enter Student Age: ").strip()
+        if age == "":
+            print("Please enter an age first...")
+        else:
+            break
+
+    while True:
+        course = input("Enter Course: ").strip()
+        if course == "":
+            print("Please enter a course first...")
+        else:
+            break
 
     with open ("student.csv","a",newline="")as file:
         writer = csv.writer(file)
